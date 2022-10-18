5657bd6fe99f99932809033c56ee4656c7ef735a
HumXC <Hum-XC@outlook.com>
2022-10-18 11:03:25 +0800

    balabala

--- hypr/scripts/bin/swww-randomize.sh
+++ hypr/scripts/bin/swww-randomize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # origin: swww example script
 # This script will randomly go through the files of a directory, setting it
 # up as the wallpaper at regular intervals
@@ -9,10 +10,16 @@ dir=$HOME/.config/hypr/wallpaper
 # Edit bellow to control the images transition
 export SWWW_TRANSITION_FPS=60
 export SWWW_TRANSITION_STEP=2
-sleep 3
+sleep 0.5
 # This controls (in seconds) when to switch to the next image
-INTERVAL=300
-
+INTERVAL=120
+setImg(){
+	swww img "$1"
+	if [ $? -ne 0 ];then
+		sleep 0.1
+		setImg "$1"
+	fi
+}
 while true; do
 	find "$dir" \
 		| while read -r img; do
@@ -20,7 +27,8 @@ while true; do
 		done \
 		| sort -n | cut -d':' -f2- \
 		| while read -r img; do
-			swww img "$img"
+			setImg "$img"
 			sleep $INTERVAL
 		done
 done
+
--- waybar/set_light.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-now=$(light -G)
-i=${now%.*}
-f=${now#*.}
-if [ "$1" == "up" ]; then
-	doas light -S `expr $i + 1`.$f
-elif [ "$1" == "down" ]; then
-	doas light -S `expr $i - 1`.$f
-fi
